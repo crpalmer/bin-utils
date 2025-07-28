380db9814016e3f1a8ad6d16b6a013ae01eb6fe7
Christopher R. Palmer <crpalmer@gmail.com>
2025-07-28 07:59:23 -0400

    read-pg: Collect the accumulated text on </p> and </h2>
    I was potentially losing the last paragraph of an article.

--- read-pg.py
+++ read-pg.py
@@ -8,18 +8,29 @@ import textwrap
 class HTMLFilter(HTMLParser):
     text = ""
     p = ""
+
     def handle_data(self, data):
         self.p += data
+
     def handle_starttag(self, tag, attrs):
         if tag == "p" or tag == "h2":
-            self.text += textwrap.fill(f.p, width = 80)
-            self.text += '\n\n'
-            self.p = ""
+            self.collect()
         if tag == "h2":
             self.p += " *** "
+
     def handle_endtag(self, tag):
         if tag == "h2":
             self.p += " *** "
+        if tag == "p" or tag == "h2":
+            self.collect()
+
+    def collect(self):
+        self.p = self.p.strip()
+        if len(self.p) > 0:
+            self.text += textwrap.fill(f.p, width = 80)
+            self.text += '\n\n'
+            self.p = ""
+
     def get_text(self):
         return text + textwrap.fill(f.p, width = 80)
 
@@ -30,10 +41,10 @@ for article in data['articles']:
   if 'pubDate' in article:
     print(article['pubDate'], end='')
     if 'contentModified' in article:
-      print(" (modified " + article['contentModified'] + ")", end='')
-    print()
+      print(" (modified " + article['contentModified'] + ")")
 
-  # print(article['body'])
+  print()
+
   f = HTMLFilter()
   f.feed(article['body'])
   print(f.text)
