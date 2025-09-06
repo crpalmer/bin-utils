743534b3e27010df4a36ec5086e192c958926de8
Christopher R. Palmer <crpalmer@gmail.com>
2025-09-06 10:19:26 -0400

    kernel-status: report of /boot/loader/entries as well

--- kernel-status.py
+++ kernel-status.py
@@ -16,31 +16,55 @@ def load_directory(dir, prefix = "", suffix = ""):
                     all.append(key)
     return dict
 
+def find_prefix(dir = "/boot/loader/entries"):
+    dict = {}
+    best = ""
+    n_best = 0
+    for file in os.listdir(dir):
+        if not os.path.isdir(os.path.join(dir, file)):
+            prefix = file.partition("-")[0]
+            if not prefix in dict:
+                dict[prefix] = 0
+            dict[prefix] = dict[prefix] + 1
+            if dict[prefix] > n_best:
+                best = prefix
+                n_best = dict[prefix]
+    return best + "-"
+
 vmlinuz = load_directory("/boot", "vmlinuz-")
 modules = load_directory("/usr/lib/modules")
 initrd = load_directory("/boot", "initramfs-", ".img")
 config = load_directory("/boot", "config-")
+loader = load_directory("/boot/loader/entries", find_prefix(), ".conf")
 
+print(loader)
 print("Installed kernels:")
 print("==================")
 
 all.sort()
 broken = []
 for version in all:
-    if version in vmlinuz.keys() and version in modules.keys() and version in initrd.keys():
+    if version in vmlinuz and version in modules and version in initrd and version in loader:
         print(version)
     else:
         broken.append(version)
+
+def report(version, bool):
+    if (version in vmlinuz) == bool:
+        print(" vmlinuz", end="")
+    if (version in modules) == bool:
+        print(" modules", end="")
+    if (version in initrd) == bool:
+        print(" initramfs", end="")
+    if (version in loader) == bool:
+        print(" boot/loader/entries", end="")
 
 if len(broken) > 0:
     print("\nBroken kernels:")
     print("=================")
     for version in broken:
-        print(version, end=" - missing:")
-        if not version in vmlinuz.keys():
-            print(" vmlinuz", end="")
-        if not version in modules.keys():
-            print(" modules", end="")
-        if not version in initrd.keys():
-            print(" initramfs", end="") 
+        print(version, end=" - has:")
+        report(version, True)
+        print(" | missing:", end="")
+        report(version, False)
         print()
