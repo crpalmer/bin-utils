2006ef661b28365ab0cac53da3eb3a1ce312757d
Christopher R. Palmer <crpalmer@gmail.com>
2025-09-20 09:16:00 -0400

    kernel-building-packages: Add in the vim/cmake and Fedora kernel.spec

--- kernel-building-packages.sh
+++ kernel-building-packages.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
 
 echo "General compilation:"
-sudo dnf install make ccache flex bison openssl-devel elfutils-libelf-devel ncurses-devel pahole dwarves
+sudo dnf install make ccache flex bison openssl-devel elfutils-libelf-devel ncurses-devel pahole dwarves cmake
 echo "Rust:"
 sudo dnf install rust rust-src bindgen-cli rustfmt clippy
+echo "VIM:"
+sudo dnf install vim
+echo "Fedora Kernel spec"
+sudo dnf install fedpkg
+mkdir -p ~/fedora && cd ~/fedora && fedpkg clone -a kernel && cd kernel && sudo dnf builddep kernel.spec
