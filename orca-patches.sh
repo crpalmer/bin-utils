c336e885baec5000a6c1591e78d0881aeba22316
Christopher R. Palmer <crpalmer@gmail.com>
2026-06-16 10:42:38 -0400

    orca-patches: minimal version as a starting point on a new machine
    Note: This doesn't get installed.  It is most likely just going to
    be constantly modified after install anyway so don't overwrite it.

--- orca-patches.sh
+++ orca-patches.sh
@@ -4,22 +4,14 @@ echo " === Syncing repos ==="
 
 git fetch orca
 git fetch me
-git fetch kisslorand
-git fetch ianalexis
 
 git reset --hard orca/main
 
 echo " === Apply patches ==="
 
+# Current pull requests
+
 # My changes
 gc-series orca/main..me/docker
 gc-series orca/main..me/build-changes
 gc-series orca/main..me/my-changes
-gc-series orca/main..me/extruder-variant
-
-# Current pull requests
-#echo "*** WARNING *** using my rebase of kisslorand's support patches"
-#gc-series orca/main..me/kisslorand-supports # rebase of:
-gc-series orca/main..kisslorand/Fix-support-interface-contact-semantics-and-gap-handling    # issue 11812
-
-#gc-series orca/main..ianalexis/waylan-bug
